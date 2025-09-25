d3f79277eecd3a171605f9fcc185229c6f372216
rakaagus <kakakunyukya@gmail.com>
2025-09-25 17:51:26 +0700

    [Feat] add entier screen

---
+++ lib/presentation/bloc/splash/bloc/JD_splash_event.dart
@@ -0,0 +1,11 @@
+import 'package:justdo_mini_project/utils/base/base_bloc_event.dart';
+
+class DoLoadSessionEvent extends BaseBlocEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class DoLoadAppInfoEvent extends BaseBlocEvent {
+  @override
+  List<Object?> get props => [];
+}
